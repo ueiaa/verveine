fd8b81087df6198d349ed6eaf95370acc2992c38
ueiaa <ueiaa0712@163.com>
2024-03-12 17:46:12 +0800

    0312

---
+++ src/components/Person-ref&reactive.vue
@@ -0,0 +1,50 @@
+<template>
+<!-- html -->
+    <div class="person">
+        <h2>一辆{{ car.brand }}车，价值{{ car.price }}</h2>
+        <button @click="changePrice">修改汽车价格</button>
+        <br>
+        <button @click="changeCar">修改汽车</button>
+    </div>
+</template>
+
+<!-- <script lang="ts">
+// JS或TS
+    export default{
+        name:'Person',
+    }
+</script> -->
+
+<script lang="ts" setup name="Person">
+    import {ref} from 'vue'
+    import {reactive} from 'vue'
+    let car = reactive({brand:'benz', price:100})
+    let games = reactive([
+        {id:'a01',name:'01'},
+        {id:'a02',name:'02'},
+        {id:'a03',name:'03'},
+    ])
+
+    function changePrice(){
+        car.price += 20
+    }
+    function changeCar(){
+        //car被reactive包裹，需要使用Object.assign
+        Object.assign(car,{brand:'auto', price:10})
+        //car被ref包裹，可以直接整个改
+        //car.value = {brand:'auto', price:10}
+    }
+</script>
+
+<style>
+/* 样式 */
+.person{
+    background-color: skyblue;
+    border-radius: 20px;
+    font-size: large;
+}
+button{
+    border-radius: 10px;
+    size: 50px 30px;
+}
+</style>
---
+++ src/components/Person-toRef&toRefs.vue
@@ -0,0 +1,51 @@
+<template>
+<!-- html -->
+    <div class="person">
+        <h2>姓名：{{ person.name }}</h2>
+        <h2>年龄：{{ person.age }}</h2>
+        <button @click = "changeName">修改姓名</button>
+        <button @click = "changeAge">修改年龄</button>
+    </div>
+</template>
+
+<!-- <script lang="ts">
+// JS或TS
+    export default{
+        name:'Person',
+    }
+</script> -->
+
+<script lang="ts" setup name="Person">
+    import {ref} from 'vue'
+    import {reactive,toRefs,toRef} from 'vue'
+
+    //定义
+    let person = reactive({name:'张三',age:18})
+    
+    //解构
+    let {name,age} = toRefs(person)
+    //toRefs将由reactive包裹的对象变成ref包裹的，批量
+
+    let nl = toRef(person,'age')
+    //toRef解构一个单独的属性
+
+    function changeName(){
+        name.value += '~'
+    }
+    function changeAge(){
+        age.value += 2
+    }
+</script>
+
+<style>
+/* 样式 */
+.person{
+    background-color: skyblue;
+    border-radius: 20px;
+    font-size: large;
+}
+button{
+    border-radius: 10px;
+    size: 50px 30px;
+}
+</style>
